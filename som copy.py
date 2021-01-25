1fbab8b66b75ca472a96679e158213c483a0531e
vinayonchip <vinayv@onchiptech.com>
2021-01-25 23:40:12 +0530

    minior updates

--- som copy.py
+++ som copy.py
@@ -28,75 +28,52 @@ class SOM(tf.keras.layers.Layer):
             
         input_vector, weights = inputs
 
-        # input_vector shape -> [batch_size, m * n, dim]
-        input_vector = tf.tile(tf.expand_dims(input_vector, axis=1), [1, self.m * self.n, 1])
+        input_vector = pack(values=[input_vector for _ in range(self.m * self.n)])
         
-        # weights shape -> [1, m * n, dim]
-        weights = tf.expand_dims(weights, axis=0)
-        
-        # diff shape -> [batch_size, m * n, dim]
+        # Training Operation  # tf.pack result will be [ (m*n),  dim ]
         diff = tf.math.subtract(weights, input_vector)
-
-        # distance shape -> [batch_size, m * n]
-        distance = tf.math.sqrt(tf.reduce_sum(tf.math.pow(diff, 2), axis=2))
-
-        # bmu_index shape -> [batch_size, m * n]
-        bmu_index = tf.math.argmin(distance, axis=1) 
+        distance = tf.math.sqrt(tf.reduce_sum(tf.math.pow(diff, 2), axis=1))
+        bmu_index = tf.math.argmin(distance, 0) 
                 
-        #slice_input = tf.pad(tf.reshape(bmu_index, [1]), np.array([[0, 1]]))
-        #bmu_loc = tf.reshape(tf.slice(self.location_vects, slice_input, 
-        #            tf.constant(np.array([1, 2]), dtype=tf.int64) ), [2])                                    
-        bmu_loc = tf.reshape(tf.gather(self.location_vects, bmu_index), [-1, 2])
+        slice_input = tf.pad(tf.reshape(bmu_index, [1]), np.array([[0, 1]]))
+        bmu_loc = tf.reshape(tf.slice(self.location_vects, slice_input, 
+                    tf.constant(np.array([1, 2]), dtype=tf.int64) ), [2])                                    
+
         if training == False:
           return bmu_loc
 
         # Iteration number
         epoch = tf.cast(epoch, tf.float32)
-        
 
         # To compute the alpha and sigma values based on iteration number
         learning_rate_op = tf.math.subtract(1.0, tf.math.divide(epoch, self.num_epochs))
         alpha_op = tf.math.multiply(self.alpha, learning_rate_op)
         sigma_op = tf.math.multiply(self.sigma, learning_rate_op)
 
-        # Start by getting the squared difference between each BMU location and every other unit in the map
-        # bmu_loc is [batch_size, 2], i.e. the coordinates of the BMU for each input vector.
-        # location vects shape should be [1, num_neurons, 2]
-        # bmu_loc should be [batch_size, 1, 2]
-        # Output needs to be [batch_size, num_neurons], i.e. a row vector of distances for each input item
-        bmu_distance_squares = tf.reduce_sum(input_tensor=tf.math.pow(tf.math.subtract(
-                tf.expand_dims(self.location_vects, axis=0),
-                tf.expand_dims(bmu_loc, axis=1)), 2), axis=2)
+        # learning rates for all neurons, based on iteration number and location w.r.t. BMU.
+        diff = tf.math.subtract(self.location_vects, pack( values=[bmu_loc for _ in range(self.m * self.n)] ) )
+        bmu_distance_squares = tf.reduce_sum(tf.math.pow(diff, 2), axis=1)
 
-        # location vects shape should be [1, num_neurons, 2]
-        # bmu_loc should be [batch_size, 1, 2]
-        # diff should be [batch_size, num_neurons, 2]
-        diff = tf.math.subtract(tf.expand_dims(self.location_vects, axis=0),  tf.expand_dims(bmu_loc, axis=1))
-        
-        # bmu_distance_squares -> [batch_size, num_neurons]
-        bmu_distance_squares = tf.cast(tf.reduce_sum(tf.math.pow(diff, 2), axis=2), tf.float32)
-        bmu_distance = tf.math.divide(bmu_distance_squares, tf.math.pow(sigma_op, 2))
-        neighbourhood_func = tf.math.exp(tf.math.negative(bmu_distance))
-        
-        # learning_rate_op -> [batch_size, num_neurons]
+        neighbourhood_func = tf.math.exp(tf.math.negative(tf.math.divide(tf.cast(
+            bmu_distance_squares, tf.float32), tf.math.pow(sigma_op, 2))))
+
         learning_rate_op = tf.math.multiply(alpha_op, neighbourhood_func)
-        learning_rate_multiplier = tf.expand_dims(learning_rate_op, axis=-1)
+
         # Finally, the op that will use learning_rate_op to update the weightage vectors of all neurons
-        #learning_rate_multiplier = pack(values=[tf.tile(tf.slice(
-        #    learning_rate_op, np.array([i]), np.array([1])), [self.dim]) for i in range(self.m * self.n)] )
+        learning_rate_multiplier = pack(values=[tf.tile(tf.slice(
+            learning_rate_op, np.array([i]), np.array([1])), [self.dim]) for i in range(self.m * self.n)] )
 
         ### Strucutre of updating weight ###
         ### W(t+1) = W(t) + W_delta ###
         ### wherer, W_delta = L(t) * ( V(t)-W(t) ) ###
 
         # W_delta = L(t) * ( V(t)-W(t) )
-        #weightage_delta -> [batch_size, num_neurons, dim]
         weightage_delta = tf.math.multiply(
             learning_rate_multiplier,
             tf.math.subtract(input_vector, weights))
 
         # W(t+1) = W(t) + W_delta
-        new_weights = tf.reduce_mean(tf.math.add(weights, weightage_delta), axis=0)
+        new_weights = tf.math.add(weights, weightage_delta)
 
         return new_weights
         
@@ -117,7 +94,7 @@ if __name__ == "__main__":
         m = 20
         n = 20
        
-        X = np.ones((1,dim)).astype(np.float32)
+        X = np.ones((dim,)).astype(np.float32)
 
         # Build the SOM object and place all of its ops on the graph
         som = SOM(m=m, n=n, dim=dim, num_epochs=20, name='SOM')
@@ -125,8 +102,8 @@ if __name__ == "__main__":
         @tf.function
         def train_graph(x, weights, epoch):
             inputs = (x, weights)
-            new_weights = som(inputs, training=True, epoch=epoch)
-            return new_weights
+            loc, new_weights = som(inputs, training=True, epoch=epoch)
+            return loc, new_weights
 
         @tf.function
         def infer_graph(x, weights):
@@ -136,7 +113,8 @@ if __name__ == "__main__":
 
         print('Training...')
         weigths = np.random.normal(size=m * n * dim).reshape((m * n, dim))
-        new_weights = train_graph(X, weigths, 0)        
+        loc, new_weights = train_graph(X, weigths, 0)
+        print('loc: ', loc.shape)
         print('new_weights: ', new_weights.shape)
         print('new_weights: ', new_weights)
         
